4ed1ce66cf8bde94a294c420b6b0d7467a3cce1c
Andrew Baxter <afb2@wustl.edu>
2016-08-09 15:16:55 +0900

    Simplified

--- httplogging.py
+++ httplogging.py
@@ -1,14 +1,11 @@
 import logging
 
 from scrapy import signals
-from scrapy.exceptions import NotConfigured
 
 
 class HttpLogging(object):
 
     def __init__(self, crawler):
-        if not crawler.settings.getbool('HTTPLOGGING_ENABLED'):
-            raise NotConfigured
         self.logger = logging.getLogger('httplogging')
         crawler.signals.connect(
             self.log,
